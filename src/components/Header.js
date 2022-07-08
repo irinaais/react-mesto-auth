9f1652444ed39f9711f3cb9a387e22958599997f
Iris <irinaais90@yandex.ru>
2022-07-08 16:51:19 +0300

    fix: removed duplicate code in Header.js and InfoTooltip.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,23 @@ import {Link} from "react-router-dom";
 import React from "react";
 
 function Header(props) {
-  if (props.loggedIn) {
-    return (
-      <header className="header page__section">
-        <img
-          className="header__logo"
-          src={logo}
-          alt="Логотип проекта Место"
-        />
-        <div className={"header__container"}>
-          <p className={"header__container_user-email"}>{props.email}</p>
-          <button className={"button header__container_button"} onClick={props.onLogout}>{props.text}</button>
-        </div>
-      </header>
-    );
-  } else {
-    return (
-      <header className="header page__section">
-        <img
-          className="header__logo"
-          src={logo}
-          alt="Логотип проекта Место"
-        />
-        <div className={"header__container"}>
-          <Link to={props.link} className="button header__container_link-text">{props.text}</Link>
-        </div>
-      </header>
-    )
-  }
+  return (
+    <header className="header page__section">
+      <img
+        className="header__logo"
+        src={logo}
+        alt="Логотип проекта Место"
+      />
+      <div className={"header__container"}>
+        {props.loggedIn ?
+          <>
+            <p className={"header__container_user-email"}>{props.email}</p>
+            <button className={"button header__container_button"} onClick={props.onLogout}>{props.text}</button>
+          </>
+        : <Link to={props.link} className="button header__container_link-text">{props.text}</Link>}
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -4,30 +4,24 @@ import React from 'react';
 
 function InfoTooltip(props) {
   function handleOverlayClick(evt) {
-    if (evt.target===evt.currentTarget) {props.onClose()}
+    if (evt.target === evt.currentTarget) {
+      props.onClose()
+    }
   }
 
-  if (props.isInfoTooltipOk) {
-    return (
-      <div className={`popup popup_infotooltip ${props.isOpen && 'popup_opened'}`} onClick={handleOverlayClick}>
-        <div className="popup__infotooltip">
-          <img src={successfulRegistration} className="popup__infotooltip-image" alt="успешная регистрация"/>
-          <h2 className="popup__infotooltip-text">Вы успешно зарегистрировались!</h2>
-          <button className="button popup__close-button" onClick={props.onClose} type="button" aria-label="Закрыть попап"/>
-        </div>
+  return (
+    <div className={`popup popup_infotooltip ${props.isOpen && 'popup_opened'}`} onClick={handleOverlayClick}>
+      <div className="popup__infotooltip">
+        <img src={props.isInfoTooltipOk ? successfulRegistration : unSuccessfulRegistration}
+             className="popup__infotooltip-image"
+             alt={props.isInfoTooltipOk ? "успешная регистрация" : "ошибка при регистрации"}/>
+        <h2
+          className="popup__infotooltip-text">{props.isInfoTooltipOk ? "Вы успешно зарегистрировались!" : "Что-то пошло не так! Попробуйте еще раз."}</h2>
+        <button className="button popup__close-button" onClick={props.onClose} type="button"
+                aria-label="Закрыть попап"/>
       </div>
-    )
-  } else {
-    return (
-      <div className={`popup popup_infotooltip ${props.isOpen && 'popup_opened'}`} onClick={handleOverlayClick}>
-        <div className="popup__infotooltip">
-          <img src={unSuccessfulRegistration} className="popup__infotooltip-image" alt="ошибка при регистрации"/>
-          <h2 className="popup__infotooltip-text">Что-то пошло не так! Попробуйте еще раз.</h2>
-          <button className="button popup__close-button" onClick={props.onClose} type="button" aria-label="Закрыть попап"/>
-        </div>
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default InfoTooltip;
